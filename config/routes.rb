19f2201428472923f808ec9a1bf4b9ee2783c251
delaney burke <delaney@zero2one.ee>
2021-09-12 12:14:35 +0300

    fix the broken routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Aiia::Engine.routes.draw do
   get '/splash', to: "endpoint#splash"
   get '/webhook', to: "endpoint#webhook"
-  get '/callback' to: "endpoint#callback"
+  get '/callback', to: "endpoint#callback"
   root to: "endpoint#splash"
 end
