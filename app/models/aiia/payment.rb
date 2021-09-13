828edfd461a65cb9d31cf1641c0b21bd2b06566a
delaney burke <delaney@zero2one.ee>
2021-09-14 02:24:45 +0300

    add to_i context

--- app/models/aiia/payment.rb
+++ app/models/aiia/payment.rb
@@ -1,7 +1,7 @@
 module Aiia
   class Payment < ApplicationRecord
 		def pay!
-	    credit = ::Credit.find(self.context)	
+			credit = ::Credit.find(self.context.to_i)	
 			credit.publish_webhooks
 		end
   end
